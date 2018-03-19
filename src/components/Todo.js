17bd06e582de8602d89f7523c050f82d73e58aa5
Tabish Khan <tabish.khan@widerfunnel.com>
2018-03-19 14:15:03 -0700

    Remove item working

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 
 class Todo extends Component {
+
+    removeItem() {
+        this.props.removeItem(this.props.todoIndex)
+    }
+
     render() {
         return (
-            <div>{this.props.value}</div>
-            // <div>This is the todo</div>
+            <div>
+                <div>{this.props.value}<input type="checkbox" onChange={ this.removeItem.bind(this) } /></div>
+            </div>
         )
     }
+
 }
 
 export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import Todo from './Todo.js';
 
+// NEED TO DELETE ITEM ON CLICK OF CHECKBOX
+// GET ITEM CLOSES TO CHECKBOX REMOVE IT FROM LISTS (PASS KEY UP TO PARENT, REMOVE FROM ARRAY)
+// HANDLE BUG WHERE IF YOU CLICK ADD MULTIPLE TIMES (EMPTY INPUT), THEN FILL INPUT IN, THEN CLICK ADD, 
+// ITLL RENDER A BUNCH OF EMPTY VALUED TODO ITEMS
+
 class TodoList extends Component {
 
     constructor(props) {
@@ -8,43 +13,94 @@ class TodoList extends Component {
         super(props)
 
         this.state = {
-            isItemAdded: false,
             itemList: []
         }
 
         this.addItem = this.addItem.bind(this);
+        this.removeItem = this.removeItem.bind(this);
+
+    }
+
+    removeItem(itemId) {
+        console.log('triggered');
+        console.log(this.state.itemList[itemId]);
+
+        // let newItem = itemId;
+
+        let filterdArray = this.state.itemList.filter((e, i) => {
+
+            return e !== this.state.itemList[itemId]
+            
+        });
+
+        this.setState({
+
+            itemList: filterdArray
+            // isItem: true,
+            // itemValue: value
+            // // debugger
+            // itemList: this.state.itemList.filter((item)=> {
+            //     // return item !== itemId  
+            //     let newItem = this.state.itemList[itemId];
+
+            //     debugger;
+            // })
+
+
+        })
 
     }
     
     addItem(e) {
-        // console.log('cliccked');
         let value = this.refs.inputbox.value;
-        // console.log(value);
-        this.state.itemList.push(value);
         if (value) {
-
+            //const { itemList } = this.state;
+            const tester = this.state.itemList;
+            // Create new array
+            const newArr1 = [];
+            // Add existing tasks to new array
+            // newArr1 = tester;
+            // tester.map((element, index) => {
+            //     console.log("element is >>" + element)
+            //     newArr1.push(element);
+            // });
+            tester.forEach(function(e, i) {
+                newArr1.push(e);
+            });
+            // Add new task to new Array
+            newArr1.push(value);
+            // setState will have itemList: newArray
             this.setState({
-                isItemAdded: true,
-                itemValue: value
+                itemList: newArr1
             })
 
+            // console.log(tester);
+            // console.log(this.state.itemList);
+
+            // this.state.itemList.push(value);
+            // this.setState({
+            //     // isItem: true,
+            //     // itemValue: value,
+            //     // itemList: [...this.state.itemList, value]
+            //     itemList: this.state.itemList
+            // })
             this.refs.inputbox.value = ''
+
         }
     }
 
     render() {
+
+        console.log(this.state.itemList);
         return (
             <div>
                 <div>Todo List</div>
-                {/* <Todo /> */}
                 <div>Enter  new item:</div>
                 <input name="input" type="text" ref="inputbox" style={{border: '1px solid black'}} />
                 <button type="button" style={{border: '1px solid black'}} onClick={this.addItem}>Add</button>
                 {
-                    this.state.isItemAdded && 
                     this.state.itemList.map((item, index) => {
-                        // <Todo />
-                        return <Todo value={ item } key={ index } />
+                        return <Todo value={ item } key={ index } removeItem={ this.removeItem } todoIndex = {index} />
                     })
                 }
             </div>
