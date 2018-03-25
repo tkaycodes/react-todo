46c3e6be23ea349bf6ecd3ff75c51c6a5003d71b
Tabish Khan <tabish.khan@widerfunnel.com>
2018-03-24 20:49:34 -0700

    Adding and removing state works, sync html input change with react input state

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,37 @@
+// TO DO:
+// WHEN REMOVING ITEM, CHECKBOX SHOULD NOT BE CHECKED 
+
 import React, {Component} from 'react';
 
 class Todo extends Component {
 
-    removeItem() {
-        this.props.removeItem(this.props.todoIndex)
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            checked: false
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleChange() {
+
+
+        this.setState({
+            checked: !this.state.checked
+        }, function() {
+            this.props.removeItem(this.props.itemIndex)
+        });
+
+       
     }
 
     render() {
         return (
             <div>
-                <div>{this.props.value}<input type="checkbox" onChange={ this.removeItem.bind(this) } /></div>
+                <div>{this.props.value}<input type="checkbox" onChange={ this.handleChange } checked={this.state.checked} /></div>
             </div>
         )
     }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,110 +1,79 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Todo from './Todo.js';
-
-// NEED TO DELETE ITEM ON CLICK OF CHECKBOX
-// GET ITEM CLOSES TO CHECKBOX REMOVE IT FROM LISTS (PASS KEY UP TO PARENT, REMOVE FROM ARRAY)
-// HANDLE BUG WHERE IF YOU CLICK ADD MULTIPLE TIMES (EMPTY INPUT), THEN FILL INPUT IN, THEN CLICK ADD, 
-// ITLL RENDER A BUNCH OF EMPTY VALUED TODO ITEMS
 
 class TodoList extends Component {
 
     constructor(props) {
 
-        super(props)
+        super(props);
 
         this.state = {
+            inputvalue: '',
             itemList: []
-        }
+        };
 
-        this.addItem = this.addItem.bind(this);
+        this.addNewItem = this.addNewItem.bind(this);
+        this.handleChange = this.handleChange.bind(this);
         this.removeItem = this.removeItem.bind(this);
 
-    }
+    };
 
-    removeItem(itemId) {
-        console.log('triggered');
-        console.log(this.state.itemList[itemId]);
-
-        // let newItem = itemId;
-
-        let filterdArray = this.state.itemList.filter((e, i) => {
-
-            return e !== this.state.itemList[itemId]
-            
-        });
-
+    handleChange(e) {
         this.setState({
+            inputvalue: e.target.value
+        });
+    };
 
-            itemList: filterdArray
-            // isItem: true,
-            // itemValue: value
-            // // debugger
-            // itemList: this.state.itemList.filter((item)=> {
-            //     // return item !== itemId  
-            //     let newItem = this.state.itemList[itemId];
+    addNewItem(e) {
 
-            //     debugger;
-            // })
+        if (this.state.inputvalue) {
 
-
-        })
-
-    }
-    
-    addItem(e) {
-        let value = this.refs.inputbox.value;
-        if (value) {
-            //const { itemList } = this.state;
-            const tester = this.state.itemList;
-            // Create new array
-            const newArr1 = [];
-            // Add existing tasks to new array
-            // newArr1 = tester;
-            // tester.map((element, index) => {
-            //     console.log("element is >>" + element)
-            //     newArr1.push(element);
-            // });
-            tester.forEach(function(e, i) {
-                newArr1.push(e);
-            });
-            // Add new task to new Array
-            newArr1.push(value);
-            // setState will have itemList: newArray
             this.setState({
-                itemList: newArr1
-            })
+                itemList: [...this.state.itemList, this.state.inputvalue],
+                inputvalue: ''
+            });
+        }
 
-            // console.log(tester);
-            // console.log(this.state.itemList);
+    };
+    
+    removeItem(itemId) {
 
-            // this.state.itemList.push(value);
-            // this.setState({
-            //     // isItem: true,
-            //     // itemValue: value,
-            //     // itemList: [...this.state.itemList, value]
-            //     itemList: this.state.itemList
-            // })
-            this.refs.inputbox.value = ''
+    
+        let filteredArray = this.state.itemList;
+
+        for (var i=0;i<filteredArray.length; i++) {
+
+            if (filteredArray[itemId] === filteredArray[i]) {
+                filteredArray.splice(i, 1);
+            }
 
         }
+
+        this.setState({
+            itemList: filteredArray
+        });
+
     }
+
 
     render() {
 
-        console.log(this.state.itemList);
         return (
             <div>
                 <div>Todo List</div>
                 <div>Enter  new item:</div>
-                <input name="input" type="text" ref="inputbox" style={{border: '1px solid black'}} />
-                <button type="button" style={{border: '1px solid black'}} onClick={this.addItem}>Add</button>
+
+                <input onChange={this.handleChange} value={this.state.inputvalue} />
+                <button onClick={this.addNewItem}>Add</button>
+
                 {
                     this.state.itemList.map((item, index) => {
-                        return <Todo value={ item } key={ index } removeItem={ this.removeItem } todoIndex = {index} />
+                        return <Todo key={index} value={item} itemIndex={index} removeItem={this.removeItem} />
                     })
                 }
+
             </div>
-        )
+        );
     }
 
 }
